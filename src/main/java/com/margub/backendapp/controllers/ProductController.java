727e28753fc71f744b69a32a0c82ff5273fe71d2
Merghoob Khan <merghoob_khan@mentor.com>
2022-07-17 01:16:51 +0530

    API to create new product

--- src/main/java/com/margub/backendapp/controllers/ProductController.java
+++ src/main/java/com/margub/backendapp/controllers/ProductController.java
@@ -1,9 +1,18 @@
 package com.margub.backendapp.controllers;
 
+import com.margub.backendapp.config.APIResponse;
+import com.margub.backendapp.dto.product.ProductDto;
+import com.margub.backendapp.models.Category;
 import com.margub.backendapp.services.CategoryService;
 import com.margub.backendapp.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController("/product")
 public class ProductController {
@@ -13,4 +22,15 @@ public class ProductController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @PostMapping("/add")
+    public ResponseEntity<APIResponse> addProduct(@RequestBody ProductDto productDto) {
+        Category category = categoryService.readCategory(productDto.getCategoryId());
+        if (Objects.isNull(category)) {
+            return new ResponseEntity<>(new APIResponse(false, "category is invalid"), HttpStatus.CONFLICT);
+        }
+
+        productService.addProduct(productDto, category);
+        return new ResponseEntity<>(new APIResponse(true, "product is added"), HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/margub/backendapp/services/ProductService.java
+++ src/main/java/com/margub/backendapp/services/ProductService.java
@@ -1,5 +1,8 @@
 package com.margub.backendapp.services;
 
+import com.margub.backendapp.dto.product.ProductDto;
+import com.margub.backendapp.models.Category;
+import com.margub.backendapp.models.Product;
 import com.margub.backendapp.reposistories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,19 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    public void addProduct(ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        productRepository.save(product);
+    }
+
+    private Product getProductFromDto(ProductDto productDto, Category category) {
+        Product product = new Product();
+        product.setCategory(category);
+        product.setDescription(productDto.getDescription());
+        product.setImageURL(productDto.getImageURL());
+        product.setName(productDto.getName());
+        product.setPrice(productDto.getPrice());
+        return product;
+    }
 }
